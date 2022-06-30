da5ff94079287fa46e78005b42f04aca52cb36be
Ol4ik88 <slapikol4ik@gmail.com>
2022-06-30 22:04:29 +0300

    refactor: install required modules and dependencies

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b4a5ea8573c44024848fb373971749e1', // получите свой ключ https://newsapi.org/
         });
     }
 }
