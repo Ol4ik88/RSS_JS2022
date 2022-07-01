dd7489140adf3720bd78643c49e94f0fba0b36cf
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-01 14:17:58 +0300

    refactor:ESLint and Webpack configure to work with TypeScript

---
+++ migration-to-ts/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'prettier',
+    'import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    'no-debugger': 'off',
+    'no-console': 0,
+    'class-methods-use-this': 'off'
+  },
+  ignorePatters: [
+    '*.html',
+    '*config.js',
+    'tsconfig.json',
+  ],
+};
--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: 'b4a5ea8573c44024848fb373971749e1', // получите свой ключ https://newsapi.org/
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: 'b4a5ea8573c44024848fb373971749e1', // получите свой ключ https://newsapi.org/
+    });
+  }
 }
 
 export default AppLoader;
