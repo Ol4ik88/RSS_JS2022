63b21bd7b946f0f7046d51421d7efa76b4b5b929
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-04 14:32:17 +0300

    fix: migrate file app.js to ts

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -1,15 +1,18 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+import { IEverything, ISources, ISource, IArticle} from '../../type/index';
 
 class App {
+  controller: AppController;
+  view: AppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
     }
 
     start() {
-        document
-            .querySelector('.sources')
+        (document
+            .querySelector('.sources')as HTMLElement)
             .addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
         this.controller.getSources((data) => this.view.drawSources(data));
     }
