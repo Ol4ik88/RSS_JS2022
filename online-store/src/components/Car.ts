a5b81722665667e7fcd8626bdcf442e7c51266ba
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-17 16:41:17 +0300

    feat: implement a Car object

---
+++ online-store/src/components/Car.ts
@@ -0,0 +1,42 @@
+import {ICar} from '../type/type';
+
+export const car: ICar = {
+  
+  productOfCar:new Set<string>(),
+  
+  add(idProduct:string){
+    let cardClick=document.querySelector(`div[data-id="${idProduct}"]`);
+    if(cardClick){
+      if(this.productOfCar.has(idProduct)){
+        this.productOfCar.delete(idProduct);
+        cardClick.classList.toggle("active");
+      } else {
+        if (this.productOfCar.size<20){
+          this.productOfCar.add(idProduct);
+            cardClick.classList.toggle("active");
+        } else {
+          alert("Car full!");
+        }
+      }
+    }
+  },
+  getSizeCar(){
+    return this.productOfCar.size;
+  },
+  renderCar(){
+    let carView=document.querySelector('.car');
+    if(carView){
+      carView.innerHTML=`<span>${this.getSizeCar()}</span>`;
+    }
+  },
+
+  updateCar(productOfCar:Set<string>){
+  let [...valueCar]=[...productOfCar];
+    productOfCar.clear();
+    valueCar.forEach((elem)=>{
+      this.add(elem);
+      this.renderCar();
+    })
+  }
+  
+}
