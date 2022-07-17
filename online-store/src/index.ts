3e79f9b81278597cf3e4cfd98dbd7b01636b6147
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-17 16:52:03 +0300

    feat: add init project

---
+++ online-store/src/index.ts
@@ -0,0 +1,33 @@
+import { Products } from './components/Products';
+import { car } from './components/Car';
+import { addClickFilterReset, initSliders } from './components/slider';
+import './global.css';
+import { addBrandClickHandler, addSearchClickHandler, addCardClickHandler, addResetSettingClickHandler, addCategoryClickHandler, addAgeClickHandler, addSaleClickHandler, filter } from './components/filter';
+import { addSortClickHandler} from './components/sorting';
+
+
+car.renderCar();
+
+export const cards = new Products();
+
+cards.filterProducts(cards.catalog, car.productOfCar);
+initSliders();
+
+window.onload=function(){
+  addCardClickHandler();
+  addSortClickHandler();
+  addSearchClickHandler();
+  addBrandClickHandler();
+  addCategoryClickHandler();
+  addAgeClickHandler();
+  addSaleClickHandler();
+  addClickFilterReset();
+  addResetSettingClickHandler();
+}
+
+
+
+
+
+
+
