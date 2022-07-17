ae986abc7df6eca8743151efc075fd273a9b34d1
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-17 20:30:46 +0300

    fix:delete space

--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,14 +2,11 @@ import { Products } from './components/Products';
 import { car } from './components/Car';
 import { addClickFilterReset, initSliders } from './components/slider';
 import './global.css';
-import { addBrandClickHandler, addSearchClickHandler, addCardClickHandler, addResetSettingClickHandler, addCategoryClickHandler, addAgeClickHandler, addSaleClickHandler, filter } from './components/filter';
+import { addBrandClickHandler, addSearchClickHandler, addCardClickHandler, addResetSettingClickHandler, addCategoryClickHandler, addAgeClickHandler, addSaleClickHandler} from './components/filter';
 import { addSortClickHandler} from './components/sorting';
 
-
 car.renderCar();
-
 export const cards = new Products();
-
 cards.filterProducts(cards.catalog, car.productOfCar);
 initSliders();
 
