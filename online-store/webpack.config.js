c2da1bf12ec49b1d7515923fc9497e29b0d69f15
Ol4ik88 <slapikol4ik@gmail.com>
2022-07-17 15:19:03 +0300

    refactor: configured config files

---
+++ online-store/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/i,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|jpg|png|svg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.sass$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js '],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new MiniCssExtractPlugin({filename: 'global.css'}),
+    new CopyPlugin({
+      patterns: [
+          {
+              from: path.resolve(__dirname, 'src/assets'),
+              to: path.resolve(__dirname, 'dist/assets'),
+          },
+      ],
+  }),
+    ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
