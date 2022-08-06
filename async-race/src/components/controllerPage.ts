fc349caf363cd86ab532596d7d7c00cf5ae3f4f9
Ol4ik88 <slapikol4ik@gmail.com>
2022-08-06 18:47:11 +0300

    feat: implement controller for page

---
+++ async-race/src/components/controllerPage.ts
@@ -0,0 +1,74 @@
+import { addlistenerGarage, updateStateGarage } from './controllerGarage';
+import { renderGarage, renderWinners } from './pageView';
+import { store } from './store';
+import { addListenerTableWinners, updateStateWinners } from './controllerWinners';
+
+const MAX_COUNT_CARS = 7;
+const MAX_COUNT_WINNERS = 10;
+
+export function updatePagination(page: number, count: number, maxCount: number) {
+  if (page * maxCount < count) {
+    (document.querySelector('#next') as HTMLButtonElement).disabled = false;
+  } else {
+    (document.querySelector('#next') as HTMLButtonElement).disabled = true;
+  }
+  if (page > 1) {
+    (document.querySelector('#prev') as HTMLButtonElement).disabled = false;
+  } else {
+    (document.querySelector('#prev') as HTMLButtonElement).disabled = true;
+  }
+}
+
+const addListenerMenu = () => {
+  const elementClick = document.querySelector('.menu') as HTMLDivElement;
+  elementClick.addEventListener('click', async (event: Event) => {
+    const element = event.target as HTMLElement;
+    if (element.classList.contains('button-view-garage')) {
+      updatePagination(store.carsPage, +store.carsCount, MAX_COUNT_CARS);
+      (document.querySelector('#garage-view') as HTMLElement).style.display = 'block';
+      (document.querySelector('#winners-view') as HTMLElement).style.display = 'none';
+    }
+    if (element.classList.contains('button-view-winners')) {
+      (document.querySelector('#garage-view') as HTMLElement).style.display = 'none';
+      (document.querySelector('#winners-view') as HTMLElement).style.display = 'block';
+      await updateStateWinners();
+      (document.querySelector('#winners-view') as HTMLElement).innerHTML = renderWinners();
+      updatePagination(store.winnersPage, +store.winnersCount, MAX_COUNT_WINNERS);
+    }
+  });
+};
+
+const addClickPaginationHandler = async (element: HTMLElement, classBtn: string) => {
+  if (element.classList.contains(`button-${classBtn}`)) {
+    switch (store.view) {
+      case 'garage': {
+        store.carsPage = classBtn === 'prev' ? store.carsPage - 1 : store.carsPage + 1;
+        await updateStateGarage();
+        (document.querySelector('#garage') as HTMLElement).innerHTML = renderGarage();
+        break;
+      }
+      case 'winners': {
+        store.winnersPage = classBtn === 'prev' ? store.winnersPage - 1 : store.winnersPage + 1;
+        await updateStateWinners();
+        (document.querySelector('#winners-view') as HTMLElement).innerHTML = renderWinners();
+        break;
+      }
+    }
+  }
+};
+
+const addListenerPagination = () => {
+  const elementClick = document.querySelector('.pagination') as HTMLDivElement;
+  elementClick.addEventListener('click', async (event: Event) => {
+    const element = event.target as HTMLElement;
+    addClickPaginationHandler(element, 'prev');
+    addClickPaginationHandler(element, 'next');
+  });
+};
+
+export const listen = () => {
+  addListenerMenu();
+  addlistenerGarage();
+  addListenerPagination();
+  addListenerTableWinners();
+};
